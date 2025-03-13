dc202e3f2b5a99de841dc42a35d574dd7c705181
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 17:57:44 -0300

    :wrench: chore: Configuração de geração de token.

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (iduser,res) => {
+    const token =  jwt.sign({ iduser}, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+    });
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return token
+}
