43a499c15b464d04e27881f6d0b87487406d4b92
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 17:59:57 -0300

    🔧 chore: Configuração do servidor.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,21 @@ import express from 'express'
 import {router} from "./routers/routes.js"
 import dotenv from 'dotenv';
 import { dbConnection} from "./lib/db.js"
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 
 dotenv.config();
 
 const app = express() 
 const port = process.env.PORT || 3001
-
-app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+}))
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb' })); 
 app.use("/v1",router)
 app.listen(port, () => {
     dbConnection()
