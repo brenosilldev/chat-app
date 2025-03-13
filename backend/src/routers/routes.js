54e546b305848f5bbaaf7121faa5875f14bde781
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 17:59:27 -0300

    ✨ feat: Rotas

--- backend/src/routers/routes.js
+++ backend/src/routers/routes.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import {Login,logout,signup} from "../controllers/auth.controller.js"
+import {Login,logout,signup,UpdateUserPhoto,checkAuth} from "../controllers/auth.controller.js"
+import { ProtecRoute } from "../middleware/auth.middleware.js";
+import { GetMessage,getUsersForSidebar ,SendMessage} from "../controllers/message.controller.js";
 
 const router = Router();
 
-
-router.post('/user/login', Login)
 router.post('/user/signup', signup)
+router.post('/user/login', Login)
+router.post('/user/logout', logout)
+router.put('/user/updatephoto',ProtecRoute, UpdateUserPhoto)
+router.get('/user/check',ProtecRoute, checkAuth)
 
+
+router.get('/message/users',ProtecRoute,getUsersForSidebar)
+router.post('/message/:id',ProtecRoute,GetMessage)
+router.post('/message/send/:id',ProtecRoute,SendMessage)
 
 
 
