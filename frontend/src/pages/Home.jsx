72e1d28cd3f6b9d1878850acea3780bea7880923
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-15 16:24:05 -0300

    :sparkles: feat: Page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React , { useEffect} from 'react'
+import { useChatStore } from '../store/useChatStore'
+import { Sidebar } from '../components/Sidebar'
+import NoChatSelected  from '../components/NoChatSelected' 
+import { ChatContainer } from '../components/ChatContainer'
+
 
 export const Home = () => {
+  const { selectedUser ,setSelectedUser} = useChatStore();
+  
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        setSelectedUser(null)
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setSelectedUser]);
+  
   return (
-    <div>Home</div>
+    <div className="h-screen bg-base-200">
+      <div className='flex pt-15'>
+        <div className='w-full max-w-7xl rounded-lg shadow-cl h-[calc(100vh-5rem)]'>
+            <div className='flex h-full overflow-hidden rounded-lg'>
+                <Sidebar />
+                {!selectedUser ? <NoChatSelected /> : <ChatContainer /> }
+  
+            </div>
+          
+        </div>
+
+      </div>
+
+
+      </div>
   )
 }
