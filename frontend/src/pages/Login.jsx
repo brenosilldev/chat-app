27ad41ae9830f2dd364c4d77aeb7be032f7420e5
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-16 09:45:52 -0300

    🚚 chore: alterado estilos no css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState} from 'react'
 import { Lock, Mail, MessageSquare, User ,Eye,EyeOff, Loader} from 'lucide-react'
 import { Link } from 'react-router-dom'
 import AuthImagePattern from '../components/AuthImagePattern'
-import { UserAuthStore } from '../store/userAuthstore'
+import { UserAuthStore } from '../store/useAuthstore'
 export const Login = () => {	
 
 	const {isLoggingIn,login} = UserAuthStore();
@@ -23,15 +23,15 @@ export const Login = () => {
   
   
   return (
-    <div className='min-h-screen grid lg:grid-cols-2'>
+    <div className='grid min-h-screen lg:grid-cols-2'>
 		<div className='flex flex-col items-center justify-center p-6 sm:p-28'>
 			<div className='w-full max-w-md space-y-8'>
-				<div className='text-center mb-8'>
+				<div className='mb-8 text-center'>
 					<div className='flex flex-col items-center gap-2 group'>
-						<div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
+						<div className='flex items-center justify-center transition-colors size-12 rounded-xl bg-primary/10 group-hover:bg-primary/20'>
 							<MessageSquare  className="size-6 text-primary"/>
 						</div>
-						<h1 className='text-2xl font-bold mt-2'>Welcome Back</h1>
+						<h1 className='mt-2 text-2xl font-bold'>Welcome Back</h1>
 						<p className='text-base-content/60'>Sign in to your account</p>
 
 					</div>
@@ -39,20 +39,20 @@ export const Login = () => {
 				<form onSubmit={'handlesubmit'} className='space-y-6'>		
 					<div className="form-control">
 						<label className="label">
-							<span className="label-text font-medium">Email</span>
+							<span className="font-medium label-text">Email</span>
 						</label>
 
 						<div className="relative">
 							{/* Ícone Mail */}
-							<div className="absolute z-999 inset-y-0 left-0 pl-3 flex items-center">
-								<Mail className="h-6 w-6 text-base-content/40" />
+							<div className="absolute inset-y-0 left-0 flex items-center pl-3 z-999">
+								<Mail className="w-6 h-6 text-base-content/40" />
 							
 							</div>
 
 							{/* Campo de Input */}
 							<input
 							type="text"
-								className="input input-bordered w-full pl-12"
+								className="w-full pl-12 input input-bordered"
 								placeholder="E-mail"
 								value={formData.email}
 								onChange={(e) => setFormData({ ...formData, email: e.target.value })}
@@ -62,10 +62,10 @@ export const Login = () => {
 					</div>
 					<div className='form-control'>
 						<label className='label'>
-							<span className='label-text font-medium'>Password</span>
+							<span className='font-medium label-text'>Password</span>
 						</label>
 						<div className='relative'>							
-							<div className='absolute z-999 inset-y-0 left-0 pl-3 flex items-center'>
+							<div className='absolute inset-y-0 left-0 flex items-center pl-3 z-999'>
 									<Lock className='size-5 text-base-content/40'></Lock>
 							
 							</div>
@@ -76,14 +76,14 @@ export const Login = () => {
 								value={formData.password}
 								onChange={(e) => setFormData({...formData,password: e.target.value})}
 							/>
-							<button type='button' className='absolute inset-y-0 right-0 pr-3 flex items-center' onClick={() => setShowPassword(!showPassword)}>
+							<button type='button' className='absolute inset-y-0 right-0 flex items-center pr-3' onClick={() => setShowPassword(!showPassword)}>
 								{showPassword ? <Eye size={20} /> : <EyeOff size={20}/>}
 							</button>
 
 						</div>
 
 					</div>
-					<button className='btn btn-primary w-full' type='submit' onClick={handlesubmit} disabled={isLoggingIn}>
+					<button className='w-full btn btn-primary' type='submit' onClick={handlesubmit} disabled={isLoggingIn}>
 						{isLoggingIn ? (
 
 							<Loader className='animate-spin'/>
