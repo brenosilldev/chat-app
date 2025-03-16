1c672bfb6d912d432c3de1c49734cea03aa848e5
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-16 09:46:30 -0300

    🚚 chore: alterado estilos no css

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState} from 'react'
-import { UserAuthStore } from '../store/userAuthstore'
+import { UserAuthStore } from '../store/useAuthstore'
 import { Lock, Mail, MessageSquare, User ,Eye,EyeOff, Loader} from 'lucide-react'
 import { Link } from 'react-router-dom'
 import AuthImagePattern from '../components/AuthImagePattern'
@@ -32,15 +32,15 @@ export const SignUpPage = () => {
 
 
   return (
-    <div className='min-h-screen grid lg:grid-cols-2'>
+    <div className='grid min-h-screen lg:grid-cols-2'>
 		<div className='flex flex-col items-center justify-center p-6 sm:p-28'>
 			<div className='w-full max-w-md space-y-8'>
-				<div className='text-center mb-8'>
+				<div className='mb-8 text-center'>
 					<div className='flex flex-col items-center gap-2 group'>
-						<div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
+						<div className='flex items-center justify-center transition-colors size-12 rounded-xl bg-primary/10 group-hover:bg-primary/20'>
 							<MessageSquare  className="size-6 text-primary"/>
 						</div>
-						<h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+						<h1 className='mt-2 text-2xl font-bold'>Create Account</h1>
 						<p className='text-base-content/60'>Get Start with your free account</p>
 
 					</div>
@@ -48,10 +48,10 @@ export const SignUpPage = () => {
 				<form onSubmit={'handlesubmit'} className='space-y-6'>
 					<div className='form-control'>
 						<label className='label'>
-							<span className='label-text font-medium'>Name</span>
+							<span className='font-medium label-text'>Name</span>
 						</label>
 						<div className='relative '>
-							<div className='absolute z-999 inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+							<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-999'>
 								<User className='size-5 text-base-content/40'  />
 
 							</div>
@@ -68,19 +68,19 @@ export const SignUpPage = () => {
 					</div>
 					<div className="form-control">
 						<label className="label">
-							<span className="label-text font-medium">Email</span>
+							<span className="font-medium label-text">Email</span>
 						</label>
 
 						<div className="relative">
 							{/* Ícone Mail */}
-							<div className="absolute z-999 inset-y-0 left-0 pl-3 flex items-center">
-								<Mail className="h-6 w-6 text-base-content/40" />							
+							<div className="absolute inset-y-0 left-0 flex items-center pl-3 z-999">
+								<Mail className="w-6 h-6 text-base-content/40" />							
 							</div>
 
 							{/* Campo de Input */}
 							<input
 							type="email"
-								className="input input-bordered w-full pl-12"
+								className="w-full pl-12 input input-bordered"
 								placeholder="E-mail"
 								value={formData.email}
 								onChange={(e) => setFormData({ ...formData, email: e.target.value })}
@@ -90,10 +90,10 @@ export const SignUpPage = () => {
 					</div>
 					<div className='form-control'>
 						<label className='label'>
-							<span className='label-text font-medium'>Password</span>
+							<span className='font-medium label-text'>Password</span>
 						</label>
 						<div className='relative'>							
-							<div className='absolute z-999 inset-y-0 left-0 pl-3 flex items-center'>
+							<div className='absolute inset-y-0 left-0 flex items-center pl-3 z-999'>
 									<Lock className='size-5 text-base-content/40'></Lock>
 							
 							</div>
@@ -104,14 +104,14 @@ export const SignUpPage = () => {
 								value={formData.password}
 								onChange={(e) => setFormData({...formData,password: e.target.value})}
 							/>
-							<button type='button' className='absolute inset-y-0 right-0 pr-3 flex items-center' onClick={() => setShowPassword(!showPassword)}>
+							<button type='button' className='absolute inset-y-0 right-0 flex items-center pr-3' onClick={() => setShowPassword(!showPassword)}>
 								{showPassword ? <Eye size={20} /> : <EyeOff size={20}/>}
 							</button>
 
 						</div>
 
 					</div>
-					<button className='btn btn-primary w-full' type='submit' onClick={handlesubmit} disabled={loading ? true : false  }>
+					<button className='w-full btn btn-primary' type='submit' onClick={handlesubmit} disabled={loading ? true : false  }>
 						{loading ? (
 
 							<Loader className='animate-spin'/>
