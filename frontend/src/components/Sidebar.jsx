9ec38b91e509b2fecd46d0f445db015bba820519
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-16 09:45:08 -0300

    🚚 chore: alterado nome da importação e implementado filtros para usuarios online,

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import  SidebarSkeleton  from './skeletons/SidebarSkeleton'
 import { User } from 'lucide-react'
-import { UserAuthStore } from '../store/userAuthstore'
+import { UserAuthStore } from '../store/useAuthstore'
 
 
 export const Sidebar = () => {
     const {getUsers,users,selectedUser,setSelectedUser,isUsersLoading} = useChatStore()
 
     const {onlineUsers} = UserAuthStore()
+    const [show,setShow] = useState(false)
+
 
     useEffect(() => {
         getUsers();
     },[getUsers])
+
+
+    const filtrededUsers = show ?  users.filter(user => onlineUsers.includes(user._id)) : users
 
 
     if(isUsersLoading) return <SidebarSkeleton />
@@ -25,12 +30,22 @@ export const Sidebar = () => {
                         <User className='w-6 h-6' />
                         <span className='hidden font-medium lg:block'>Contacts</span>
                     </div>
+                    <div className='items-center hidden gap-2 mt-3 lg:flex'>
+                        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+                            <input type="checkbox" checked={show} onChange={(e) => setShow(e.target.checked)}  className='checkbox checkbox-sm' />
+                            
+                            <span>Show online users</span>
+                            
+                        </label>
+                        <span>({onlineUsers.length - 1} online)</span>
+                        
+                    </div>
                         
                 </div>
             
             
                 <div className='w-full py-3 overflow-y-auto'>
-                    {users.map((user) => (
+                    {filtrededUsers.map((user) => (
                         <button 
                             key={user?._id}
                             className={`flex items-center w-full gap-3 p-3 ${user?._id === selectedUser?._id ? 'bg-cyan-100' : 'hover:bg-base-200/50'}`}
@@ -58,7 +73,11 @@ export const Sidebar = () => {
                             
                         </button>
                     ))}
-
+                    {filtrededUsers.length === 0 && (
+                        <div className='py-4 text-center text-zinc-500'>
+                           No users found
+                        </div>
+                    )}
                 </div>
 
 
