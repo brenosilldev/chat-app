3ef56d9a2cda7cca79e3623c8f001cbac1692120
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-16 09:45:30 -0300

    🚚 chore: alterado  para buscar a url enquanto tiver em produção

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,3 +1,5 @@
 import axios from "axios";  
 
-export const api = axios.create({ baseURL: 'http://localhost:3001/v1/',withCredentials: true });
+export const api = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3001/v1" : "/v1",
+    withCredentials: true });
