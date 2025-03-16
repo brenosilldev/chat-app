c23a4be44d0018af717903b675f90212efe664e9
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-16 09:42:43 -0300

    ✨ feat: Adicionado para renderizar o front end com o back end

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,15 @@ import dotenv from 'dotenv';
 import { dbConnection} from "./lib/db.js"
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { app,server } from './lib/socket.js';
+import path from 'path';
+
+const  __dirname = path.resolve();
 
 
 dotenv.config();
 
-const app = express() 
+ 
 const port = process.env.PORT || 3001
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -18,7 +22,17 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' })); 
 app.use("/v1",router)
-app.listen(port, () => {
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("*", (req, res) => {    
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    });
+}
+
+
+server.listen(port, () => {
     dbConnection()
     console.log('Server is running on port ' + port)
 })
